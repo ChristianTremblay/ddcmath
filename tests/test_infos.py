e0b2a7d9185c869888e4aef47f4618465317fa59
Christian Tremblay, ing <christian.tremblay@servisys.com>
2016-04-03 12:27:49 -0400

    useless test on infos to boost coverage report
    Signed-off-by: Christian Tremblay, ing. <christian.tremblay@servisys.com>

---
+++ tests/test_infos.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015 by Christian Tremblay, P.Eng <christian.tremblay@servisys.com>
+#
+# Licensed under GPLv3, see file LICENSE in this source tree.
+
+from ddcmath import infos
+
+def test_infos():
+    """
+    I hate to see 0% coverage :-)
+    """
+    assert infos.__version__
+    assert infos.__author__
+    assert infos.__email__
+    assert infos.__url__
+    assert infos.__download_url__
+    assert infos.__license__
+    
