e3069e58700e70a5aba9bb636fa1872aa25ba352
Christian Tremblay, ing <christian.tremblay@servisys.com>
2021-09-20 20:56:58 -0400

    This way I handle system of measure of data provided...  then the formula use the value it needs.

--- tests/test_psychrometric.py
+++ tests/test_psychrometric.py
@@ -1,15 +1,50 @@
-from ddcmath.psychrometric import enthalpy, dewpoint, temperature_from_humidity_and_dewpoint, humidity_from_wetbulb_and_drybulb
+from ddcmath.psychrometric import (
+    enthalpy,
+    dewpoint_from_temperature_and_humidity,
+    temperature_from_humidity_and_dewpoint,
+    humidity_from_wetbulb_and_drybulb,
+)
 from ddcmath.tolerance import abs_relative_error
+
 
 def test_enthalpy():
     pass
 
 
 def test_dewpoint():
-    assert (dewpoint(temp=20, hum=60, SI=True) - 12) < 0.01
-    assert (dewpoint(temp=40, hum=65, SI=False) - 29.22) < 0.01
+    assert (
+        dewpoint_from_temperature_and_humidity(
+            temperature=20, humidity=60, SI=True
+        ).magnitude
+        - 12
+    ) < 0.01
+    assert (
+        dewpoint_from_temperature_and_humidity(
+            temperature=40, humidity=65, SI=False
+        ).magnitude
+        - 29.22
+    ) < 0.01
 
-def enthlapy():
-    assert abs_relative_error(humidity_from_wetbulb_and_drybulb(Td=20, Tw=20),100) < 0.01
-    assert abs_relative_error(humidity_from_wetbulb_and_drybulb(Td=30, Tw=20),38.957) < 0.01
-    assert abs_relative_error(humidity_from_wetbulb_and_drybulb(Td=20, Tw=15),58.376) < 0.01
+
+def test_humidity():
+    assert (
+        abs_relative_error(
+            humidity_from_wetbulb_and_drybulb(temperature=20, wet_bulb=20).magnitude,
+            100,
+        )
+        < 0.01
+    )
+    assert (
+        abs_relative_error(
+            humidity_from_wetbulb_and_drybulb(temperature=30, wet_bulb=20).magnitude,
+            38.957,
+        )
+        < 0.01
+    )
+    assert (
+        abs_relative_error(
+            humidity_from_wetbulb_and_drybulb(temperature=20, wet_bulb=15).magnitude,
+            58.376,
+        )
+        < 0.01
+    )
